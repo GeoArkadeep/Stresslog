Fixed BHP calculation, getshmax from stress polygon needs fixin like there needs to be more nans, if no good answer exists, need to incorporate nofmi flag

src/porepressurebuddy/BoreStab.py
@@ -17,8 +17,7 @@ import math
 #theta = 37
 #deltaP = 3
 
-
-def getSigmaTT(s1,s2,s3,alpha,beta,gamma,azim,inc,theta,deltaP,nu=0.35):
+def getSigmaTT(s1,s2,s3,alpha,beta,gamma,azim,inc,theta,deltaP,Pp,nu=0.35):
     Ss = np.array([[s1,0,0],[0,s2,0],[0,0,s3]])
     #print(Ss)
 
@@ -60,7 +59,10 @@ def getSigmaTT(s1,s2,s3,alpha,beta,gamma,azim,inc,theta,deltaP,nu=0.35):
     #print(Sg)
     Sb = Rb@RsT@Ss@Rs@RbT
     #print(Sb)
-
+    Sb[0][0] = Sb[0][0] - Pp
+    Sb[1][1] = Sb[1][1] - Pp
+    Sb[2][2] = Sb[2][2] - Pp
+    
     theta = math.radians(theta)
 
     Szz = Sb[2][2] - ((2*nu)*(Sb[0][0]-Sb[1][1])*(2*math.cos(2*theta))) - (4*nu*Sb[0][1]*math.sin(2*theta))
@@ -84,7 +86,7 @@ def getSigmaTT(s1,s2,s3,alpha,beta,gamma,azim,inc,theta,deltaP,nu=0.35):
 
 
 
-def drawStab(s1,s2,s3,deltaP,UCS,alpha=0,beta=0,gamma=0):
+def drawStab(s1,s2,s3,deltaP,Pp,UCS,alpha=0,beta=0,gamma=0):
     values = np.zeros((10,37))
     inclination = np.zeros((10,37))
     azimuth = np.zeros((10,37))
@@ -95,7 +97,7 @@ def drawStab(s1,s2,s3,deltaP,UCS,alpha=0,beta=0,gamma=0):
             pointer= 0
             line = np.zeros(361)
             while pointer<361:
-                STT,SZZ,TTZ,STM,stm,omega,orit = getSigmaTT(s1,s2,s3, alpha,beta,gamma, azim*10, inc*10, pointer, deltaP)
+                STT,SZZ,TTZ,STM,stm,omega,orit = getSigmaTT(s1,s2,s3, alpha,beta,gamma, azim*10, inc*10, pointer, deltaP,Pp)
                 line[pointer] = STM
                 pointer+=1
             values[inc][azim] = np.max(line)
@@ -119,7 +121,7 @@ def drawStab(s1,s2,s3,deltaP,UCS,alpha=0,beta=0,gamma=0):
     plt.show()
 
 
-def drawBreak(s1,s2,s3,deltaP,UCS,alpha=0,beta=0,gamma=0,nu=0.35):
+def drawBreak(s1,s2,s3,deltaP,Pp,UCS,alpha=0,beta=0,gamma=0,nu=0.35):
     values = np.zeros((10,37))
     inclination = np.zeros((10,37))
     azimuth = np.zeros((10,37))
@@ -131,7 +133,7 @@ def drawBreak(s1,s2,s3,deltaP,UCS,alpha=0,beta=0,gamma=0,nu=0.35):
             line = np.zeros(361)
             width = 0
             while pointer<361:
-                STT,SZZ,TTZ,STM,stm,omega,orit = getSigmaTT(s1,s2,s3, alpha,beta,gamma, azim*10, inc*10, pointer, deltaP,nu)
+                STT,SZZ,TTZ,STM,stm,omega,orit = getSigmaTT(s1,s2,s3, alpha,beta,gamma, azim*10, inc*10, pointer, deltaP,Pp,nu)
                 if (STT-UCS)>0:
                     width+=1
                 pointer+=1
@@ -173,7 +175,7 @@ def drawBreak(s1,s2,s3,deltaP,UCS,alpha=0,beta=0,gamma=0,nu=0.35):
     plt.title( "UCS = "+str(UCS)+", DeltaP = "+str(deltaP)+", Nu = "+str(nu) , loc="center")
     plt.show()
     
-def drawDITF(s1,s2,s3,deltaP,alpha=0,beta=0,gamma=0,offset=0,nu=0.35):
+def drawDITF(s1,s2,s3,deltaP,Pp,alpha=0,beta=0,gamma=0,offset=0,nu=0.35):
     values = np.zeros((10,37))
     inclination = np.zeros((10,37))
     azimuth = np.zeros((10,37))
@@ -188,7 +190,7 @@ def drawDITF(s1,s2,s3,deltaP,alpha=0,beta=0,gamma=0,offset=0,nu=0.35):
             frac = np.zeros(361)
             widthR = np.zeros(361)
             while pointer<361:
-                STT,SZZ,TTZ,STM,stm,omega,orit = getSigmaTT(s1,s2,s3, alpha,beta,gamma, azim*10, inc*10, pointer, deltaP)
+                STT,SZZ,TTZ,STM,stm,omega,orit = getSigmaTT(s1,s2,s3, alpha,beta,gamma, azim*10, inc*10, pointer, deltaP,Pp,nu)
                 line[pointer] = STT
                 angle[pointer] = omega
                 if STT<0:
@@ -244,7 +246,7 @@ def drawDITF(s1,s2,s3,deltaP,alpha=0,beta=0,gamma=0,offset=0,nu=0.35):
     cb.set_label("Excess Mud Pressure to TensileFrac")
     plt.show()
 
-def getHoopMin(inc,azim,s1,s2,s3,deltaP, alpha=0,beta=0,gamma=0,nu=0.35):
+def getHoopMin(inc,azim,s1,s2,s3,deltaP,Pp, alpha=0,beta=0,gamma=0,nu=0.35):
     values = np.zeros((10,37))
     
     pointer= 0
@@ -277,7 +279,7 @@ def getHoopMin(inc,azim,s1,s2,s3,deltaP, alpha=0,beta=0,gamma=0,nu=0.35):
     return np.min(line)
 
 
-def draw(s1,s2,s3,deltaP,UCS = 0,alpha=0,beta=0,gamma=0,offset=0,nu=0.35):
+def draw(s1,s2,s3,deltaP,Pp,UCS = 0,alpha=0,beta=0,gamma=0,offset=0,nu=0.35):
     values = np.zeros((10,37))
     values2 = np.zeros((10,37))
     inclination = np.zeros((10,37))
@@ -297,10 +299,10 @@ def draw(s1,s2,s3,deltaP,UCS = 0,alpha=0,beta=0,gamma=0,offset=0,nu=0.35):
             frac = np.zeros(361)
             widthR = np.zeros(361)
             while pointer<361:
-                STT,SZZ,TTZ,STM,stm,omega,orit = getSigmaTT(s1,s2,s3, alpha,beta,gamma, azim*10, inc*10, pointer, deltaP)
+                STT,SZZ,TTZ,STM,stm,omega,orit = getSigmaTT(s1,s2,s3, alpha,beta,gamma, azim*10, inc*10, pointer, deltaP,Pp,nu)
                 line[pointer] = STT
                 angle[pointer] = omega
-                if STT<TS:
+                if stm<TS:
                     width+=1
                     frac[pointer] = frac[pointer-1]+(1/math.tan(math.radians(omega)))
                 else:
@@ -313,8 +315,8 @@ def draw(s1,s2,s3,deltaP,UCS = 0,alpha=0,beta=0,gamma=0,offset=0,nu=0.35):
                 if STM>UCS:
                     width2+=0.5
                     
-            if width>0:
-                print("Width = ",width/2,", omega =",np.max(angle), " at inclination = ",inc*10, " and azimuth= ",azim*10)
+            #if width>0:
+                #print("Width = ",width/2,", omega =",np.max(angle), " at inclination = ",inc*10, " and azimuth= ",azim*10)
                 #plt.scatter(np.array(range(0,361)),frac)
                 #plt.plot(angle)
                 #plt.plot(line)
@@ -363,8 +365,8 @@ def draw(s1,s2,s3,deltaP,UCS = 0,alpha=0,beta=0,gamma=0,offset=0,nu=0.35):
     aws.set_rmax(90)
     aws.set_theta_zero_location("N")
     aws.set_theta_direction(-1)
-    levels = np.linspace(0,120,120)
-    cax2 = aws.contourf(azimuth, inclination, values2, 120, levels=levels, extend = 'max', cmap = 'jet', alpha = 1)
+    levels = np.linspace(0,120,1200)
+    cax2 = aws.contourf(azimuth, inclination, values2, 1200, levels=levels, extend = 'both', cmap = 'jet', alpha = 1)
     print(orit)
     aws.scatter(math.radians(orit[0]),orit[1], s=20, color = 'black', edgecolors='black', label=s3)
     aws.text(math.radians(orit[0]),orit[1], " "+str(round(s3,1)))

src/porepressurebuddy/DrawSP.py
@@ -24,10 +24,11 @@ def drawSP(Sv,Pp,bhp,shmin,UCS = 0,PhiB = 0, breakouts = 0, ditflag = 0,mu = 0.6
     print("Corners: ",ShmP,SHMP)
 
 
-    maxSt = 1.02*SHMP
-    minSt = 0.98*ShmP
+    #maxSt = 1.02*SHMP
+    #minSt = 0.98*ShmP
     
-    
+    maxSt = 200
+    minSt = 0
 
     fig,ax = plt.subplots()
     ax.axis([minSt,maxSt,minSt,maxSt])
@@ -60,20 +61,20 @@ def drawSP(Sv,Pp,bhp,shmin,UCS = 0,PhiB = 0, breakouts = 0, ditflag = 0,mu = 0.6
     #UCS = 46
     UCShigh = UCS + (0.2*UCS)
     UCSlow = UCS - (0.2*UCS)
-    Shm = 1
-    Shm2 = maxSt
+    Shm = ShmP
+    Shm2 = SHMP
     #PhiB = 0.1 #degrees
     PhiBr = math.radians(PhiB)
     TwoCosPhiB = 2*(math.cos((math.pi)-(PhiBr)))
     print("TwoCosPhiB: ",TwoCosPhiB)
     #ShmP = UCS
-    SHM1 = ((UCS + (2*Pp) + (bhp-Pp)) - (Shm*(1+TwoCosPhiB)))/(1-TwoCosPhiB)
-    SHM2 = ((UCS + (2*Pp) + (bhp-Pp)) - (Shm2*(1+TwoCosPhiB)))/(1-TwoCosPhiB)
+    SHM1 = ((UCS + (2*Pp) + (bhp-Pp)) - ((Shm)*(1+TwoCosPhiB)))/(1-TwoCosPhiB)
+    SHM2 = ((UCS + (2*Pp) + (bhp-Pp)) - ((Shm2)*(1+TwoCosPhiB)))/(1-TwoCosPhiB)
     
-    SHM1H = ((UCShigh + (2*Pp) + (bhp-Pp)) - (Shm*(1+TwoCosPhiB)))/(1-TwoCosPhiB)
-    SHM2H = ((UCShigh + (2*Pp) + (bhp-Pp)) - (Shm2*(1+TwoCosPhiB)))/(1-TwoCosPhiB)
-    SHM1L = ((UCSlow + (2*Pp) + (bhp-Pp)) - (Shm*(1+TwoCosPhiB)))/(1-TwoCosPhiB)
-    SHM2L = ((UCSlow + (2*Pp) + (bhp-Pp)) - (Shm2*(1+TwoCosPhiB)))/(1-TwoCosPhiB)
+    SHM1H = ((UCShigh + (2*Pp) + (bhp-Pp)) - ((Shm)*(1+TwoCosPhiB)))/(1-TwoCosPhiB)
+    SHM2H = ((UCShigh + (2*Pp) + (bhp-Pp)) - ((Shm2)*(1+TwoCosPhiB)))/(1-TwoCosPhiB)
+    SHM1L = ((UCSlow + (2*Pp) + (bhp-Pp)) - ((Shm)*(1+TwoCosPhiB)))/(1-TwoCosPhiB)
+    SHM2L = ((UCSlow + (2*Pp) + (bhp-Pp)) - ((Shm2)*(1+TwoCosPhiB)))/(1-TwoCosPhiB)
 
     #print(SHM1)
     br1 = np.array([(Shm,SHM1),(Shm2,SHM2)])
@@ -95,9 +96,11 @@ def drawSP(Sv,Pp,bhp,shmin,UCS = 0,PhiB = 0, breakouts = 0, ditflag = 0,mu = 0.6
     ax.add_patch(Breakout1)
     ax.add_patch(Breakout3)
     
-    Shm3 = 1
-    Shm4 = maxSt
+    Shm3 = ShmP
+    Shm4 = Y
     
+    #DITFshmax3 = 3*Shm3 - 2*Pp
+    #DITFshmax4 = 3*Shm4 - 2*Pp
     DITFshmax3 = (ufac*Shm3) - ((ufac-1)*Pp) - (bhp-Pp)
     DITFshmax4 = (ufac*Shm4) - ((ufac-1)*Pp) - (bhp-Pp)
     ditf = np.array([(Shm3,DITFshmax3),(Shm4,DITFshmax4)])
@@ -147,7 +150,7 @@ def drawSP(Sv,Pp,bhp,shmin,UCS = 0,PhiB = 0, breakouts = 0, ditflag = 0,mu = 0.6
     plt.show()
 
 
-def getSHMax(Sv,Pp,bhp,shmin, UCS = 0, PhiB = 45, breakouts = 0, ditflag = 0, mu=0.6):
+def getSHMax(Sv,Pp,bhp,shmin, UCS = 0, ThetaB = 45, breakouts = 0, ditflag = 0, mu=0.6):
     maxSH = 0
     minSH = 0
     midSH = 0
@@ -158,10 +161,6 @@ def getSHMax(Sv,Pp,bhp,shmin, UCS = 0, PhiB = 45, breakouts = 0, ditflag = 0, mu
     ShmP = ((Sv-Pp)/ufac)+Pp
     SHMP = ((Sv-Pp)*ufac)+Pp
     #print("Corners: ",ShmP,SHMP)
-    
-    if(shmin<ShmP):
-        return[0,0,0]
-        
 
     maxSt = 1.05*SHMP
     minSt = 0.90*ShmP
@@ -189,7 +188,7 @@ def getSHMax(Sv,Pp,bhp,shmin, UCS = 0, PhiB = 45, breakouts = 0, ditflag = 0, mu
     Shm = 1
     Shm2 = maxSt
     #PhiB = 0.1 #degrees
-    PhiBr = math.radians(PhiB)
+    PhiBr = math.radians(ThetaB)
     TwoCosPhiB = 2*(math.cos((math.pi)-(PhiBr)))
 
 
@@ -216,17 +215,20 @@ def getSHMax(Sv,Pp,bhp,shmin, UCS = 0, PhiB = 45, breakouts = 0, ditflag = 0, mu
     Shm3 = 1
     Shm4 = maxSt
 
+    #DITFshmax3 = 3*Shm3 - 2*Pp
+    #DITFshmax4 = 3*Shm4 - 2*Pp
     DITFshmax3 = (ufac*Shm3) - ((ufac-1)*Pp) - (bhp-Pp)
     DITFshmax4 = (ufac*Shm4) - ((ufac-1)*Pp) - (bhp-Pp)
     ditf = np.array([(Shm3,DITFshmax3),(Shm4,DITFshmax4)])
     lowerd = [Shm3,DITFshmax3]
     upperd = [Shm4,DITFshmax4]
     
-    
+    if(shmin<ShmP):
+        return[ShmP,ShmP,ShmP]
     if(shmin>Sv):
         minSH = Sv
         maxSH = SHMP
-        return [Sv,SHMP,(shmin+Sv)/2]
+        #return [Sv,shmin,(shmin+Sv)/2]
     else:
         lower = [ShmP,Sv]
         upper = [Sv,SHMP]
@@ -235,16 +237,23 @@ def getSHMax(Sv,Pp,bhp,shmin, UCS = 0, PhiB = 45, breakouts = 0, ditflag = 0, mu
         minSH = shmin
         maxSH = I1
         #return [shmin,I1,(shmin+I1)/2]
-
+    
+    if ditflag>0 and breakouts>0:
+        minSH = loweruhigh[1]+(shmin-loweruhigh[0])*(upperuhigh[1]-loweruhigh[1])/(upperuhigh[0]-loweruhigh[0])
+        maxSH = lowerd[1]+(shmin-lowerd[0])*(upperd[1]-lowerd[1])/(upperd[0]-lowerd[0])
+    else:
         if ditflag>0:
-            minSH = np.interp(shmin,lowerd,upperd)
+            minSH = lowerd[1]+(shmin-lowerd[0])*(upperd[1]-lowerd[1])/(upperd[0]-lowerd[0])#np.interp(shmin,lowerd,upperd)
         else:
-            maxSH = np.interp(shmin,lowerd,upperd)
+            maxSH = lowerd[1]+(shmin-lowerd[0])*(upperd[1]-lowerd[1])/(upperd[0]-lowerd[0])#np.interp(shmin,lowerd,upperd)
         if breakouts>0:
-            maxSH = np.interp(shmin,loweruhigh,upperuhigh)
-            minSH = np.interp(shmin,lowerulow,upperulow)
+            maxSH = loweruhigh[1]+(shmin-loweruhigh[0])*(upperuhigh[1]-loweruhigh[1])/(upperuhigh[0]-loweruhigh[0])
+            minSH = lowerulow[1]+(shmin-lowerulow[0])*(upperulow[1]-lowerulow[1])/(upperulow[0]-lowerulow[0])
         else:
-            maxSH = lowerucs[1]+(shmin-lowerucs[0])*(upperucs[1]-lowerucs[1])/(upperucs[0]-lowerucs[0])
-        midSH=(minSH+maxSH)/2    
+            maxSH = lowerucs[1]+(shmin-lowerucs[0])*(upperucs[1]-lowerucs[1])/(upperucs[0]-lowerucs[0])#np.interp(shmin,lowerucs,upperucs)
+            #print("nobreak")
+            #print(lowerucs,upperucs,maxSH)
+    midSH=(minSH+maxSH)/2    
     return [minSH,maxSH,midSH]
 from BoreStab import draw
+from BoreStab import drawStab

src/porepressurebuddy/app.py
@@ -826,7 +826,6 @@ def plotPPmiller(well,app_instance, rhoappg = 16.33, lamb=0.0008, a = 0.630, nu 
     print(mud_weight)
     first = [mud_weight[0][0],0]
     last = [mud_weight[-1][0],final_depth]
-    lastmw = last[0]
     frac_grad_data = app_instance.get_frac_grad_data_values()[0]
     flow_grad_data = app_instance.get_flow_grad_data_values()[0]
     frac_psi_data = app_instance.get_frac_grad_data_values()[1]
@@ -927,6 +926,7 @@ def plotPPmiller(well,app_instance, rhoappg = 16.33, lamb=0.0008, a = 0.630, nu 
     print(attrib[1])
     well.location.gl = float(attrib[1])
     well.location.kb = float(attrib[0])
+    mudweight = np.zeros(len(tvd))
     try:
         agf = (well.location.ekb-well.location.egl)*3.28084
     except AttributeError:
@@ -948,6 +948,7 @@ def plotPPmiller(well,app_instance, rhoappg = 16.33, lamb=0.0008, a = 0.630, nu 
     tvdbglf = np.zeros(len(tvdbgl))
     tvdmslf = np.zeros(len(tvdmsl))
     wdfi = np.zeros(len(tvdmsl))
+    j=1
     i=0
     while(i<len(tvd)):
         if tvdbgl[i]<0:
@@ -960,6 +961,11 @@ def plotPPmiller(well,app_instance, rhoappg = 16.33, lamb=0.0008, a = 0.630, nu 
             tvdmslf[i] = 0
         else:
             tvdmslf[i] = tvdmsl[i]*3.28084
+        if md[i]<mud_weight[j][1]:
+            mudweight[i] = mud_weight[j][0]
+        else:
+            mudweight[i] = mud_weight[j][0]
+            j+=1
         i+=1
     
     print("air gap is ",agf,"feet")
@@ -985,7 +991,7 @@ def plotPPmiller(well,app_instance, rhoappg = 16.33, lamb=0.0008, a = 0.630, nu 
             if(tvdbgl[i]>=0):
                 rhoppg[i] = rhoappg +(((tvdf[i]-agf-wdf)/3125)**a)
                 hydrostatic[i] = water
-                mudhydrostatic[i] = 1.0*lastmw
+                mudhydrostatic[i] = 1.0*mudweight[i]
             else:
                 if(tvdmsl[i]<0):
                     rhoppg[i] = 8.34540426515252*water
@@ -994,12 +1000,12 @@ def plotPPmiller(well,app_instance, rhoappg = 16.33, lamb=0.0008, a = 0.630, nu 
                 else:
                     rhoppg[i] = 0
                     hydrostatic[i] = water
-                    mudhydrostatic[i] = 1.0*lastmw
+                    mudhydrostatic[i] = 1.0*mudweight[i]
         else:
             if(tvdbgl[i]>=0):
                 rhoppg[i] = rhoappg +(((tvdbglf[i])/3125)**a)
                 hydrostatic[i]= water
-                mudhydrostatic[i] = 1.0*lastmw
+                mudhydrostatic[i] = 1.0*mudweight[i]
             else:
                 rhoppg[i] = 0
                 hydrostatic[i] = 0
@@ -1315,10 +1321,10 @@ def plotPPmiller(well,app_instance, rhoappg = 16.33, lamb=0.0008, a = 0.630, nu 
             print("reverse")
         sigmas.sort()
         
-        sigmas.append(bhpmpa)
-        sigmas = sigmas[:] - ppmpa
+        sigmas.append(bhpmpa-ppmpa)
+        sigmas.append(ppmpa)
         #drawStab(sigmas[0],sigmas[1],sigmas[2],sigmas[3],alpha,beta,gamma)
-        draw(sigmas[0],sigmas[1],sigmas[2],sigmas[3],ucsmpa,alpha,beta,gamma,offset,nu2[doiX])
+        draw(sigmas[0],sigmas[1],sigmas[2],sigmas[3],sigmas[4],ucsmpa,alpha,beta,gamma,offset,nu2[doiX])
         #drawDITF(sigmas[0],sigmas[1],sigmas[2],sigmas[3],alpha,beta,gamma)
     
     
@@ -1353,7 +1359,10 @@ def plotPPmiller(well,app_instance, rhoappg = 16.33, lamb=0.0008, a = 0.630, nu 
     well.data['SHMpsi'] =  sHMpsi
     shmpsi = Curve(ssgHMpsi, mnemonic='shmin_PRESSURE',units='psi', index=md, null=0)
     well.data['shmpsi'] =  shmpsi
-    
+    mwpsi = Curve(mudpsi, mnemonic='MUD_PRESSURE',units='psi', index=md, null=0)
+    well.data['mwpsi'] =  mwpsi
+    mhpsi = Curve(mudweight, mnemonic='MUD_GRADIENT',units='g/cc', index=md, null=0)
+    well.data['mwpsi'] =  mhpsi
     c0lalmpa = Curve(slal, mnemonic='C0_Lal',units='MPa', index=md, null=0)
     well.data['C0LAL'] =  c0lalmpa
     c0lal2mpa = Curve(slal2, mnemonic='C0_Lal_Phi',units='MPa', index=md, null=0)
@@ -1389,6 +1398,7 @@ def plotPPmiller(well,app_instance, rhoappg = 16.33, lamb=0.0008, a = 0.630, nu 
     plt2.legend()
     plt2.set_xlim([300,50])
 
+    plt3.plot(mudweight,tvd,color='brown',label='Mud Gradient')
     plt3.plot(fg,tvd,color='blue',label='Fracture Gradient (Daines)')
     #plt3.plot(fg2,tvd,color='aqua',label='Fracture Gradient (Zoback)')
     plt3.plot(pp,tvd,color='red',label='Pore Pressure Gradient (Zhang)')
@@ -1397,10 +1407,11 @@ def plotPPmiller(well,app_instance, rhoappg = 16.33, lamb=0.0008, a = 0.630, nu 
     plt3.set_xlim([0,3])
 
     plt4.plot(fgpsi,tvd,color='blue',label='Sh min')
+    plt4.plot(ssgHMpsi,tvd,color='pink',label='SH Max')
     plt4.plot(obgpsi,tvd,color='green',label='Sigma V')
     plt4.plot(hydropsi,tvd,color='aqua',label='Hydrostatic')
     plt4.plot(pppsi,tvd,color='red',label='Pore Pressure')
-    plt4.plot(psisfl,tvd,color='pink',label='Lade')
+    plt4.plot(mudpsi,tvd,color='red',label='Bottom Hole Pressure')
     plt4.legend()
     #plt4.set_xlim([0,5000])
     
@@ -1458,6 +1469,8 @@ def plotPPmiller(well,app_instance, rhoappg = 16.33, lamb=0.0008, a = 0.630, nu 
     plt1.set_xlim([300,50])
     plt1.title.set_text("Sonic (us/ft)")
 
+    
+    plt2.plot(mudweight,tvd,color='brown',label='Mud Gradient')
     plt2.plot(fg,tvd,color='blue',label='Fracture Gradient (Daines)')
     #plt3.plot(fg2,tvd,color='aqua',label='Fracture Gradient (Zoback)')
     plt2.plot(pp,tvd,color='red',label='Pore Pressure Gradient (Zhang)')
@@ -1467,11 +1480,11 @@ def plotPPmiller(well,app_instance, rhoappg = 16.33, lamb=0.0008, a = 0.630, nu 
     plt2.title.set_text("Gradients (g/cc)")
 
     plt3.plot(fgpsi,tvd,color='blue',label='Sh min')
+    plt3.plot(ssgHMpsi,tvd,color='pink',label='SH MAX')
     plt3.plot(obgpsi,tvd,color='green',label='Sigma V')
     plt3.plot(hydropsi,tvd,color='aqua',label='Hydrostatic')
     plt3.plot(pppsi,tvd,color='red',label='Pore Pressure')
-    plt3.plot(mudpsi,tvd,color='pink',label='BHP')
-    plt3.plot(ssgHMpsi,tvd,color='gray',label='SH MAX')
+    plt3.plot(mudpsi,tvd,color='brown',label='BHP')
     #plt3.plot(sgHMpsiL,tvd,color='lime',label='SH MAX L')
     #plt3.plot(sgHMpsiU,tvd,color='orange',label='SH MAX U')
     plt3.legend(fontsize = "6",loc='lower center')
@@ -1510,8 +1523,8 @@ def plotPPmiller(well,app_instance, rhoappg = 16.33, lamb=0.0008, a = 0.630, nu 
     if flow_psi_data != [[0,0]]:
         plt3.scatter(x_values4, y_values4, color='orange', marker='x', s=500)  # Add the custom plot to the second track
     
-    mud_weight_x, mud_weight_y = zip(*mud_weight)
-    plt2.plot(mud_weight_x, mud_weight_y, color='black', linewidth=2, linestyle='-', drawstyle='steps-post')  # Add the stepped mud_weight line to the second track
+    #mud_weight_x, mud_weight_y = zip(*mud_weight)
+    #plt2.plot(mud_weight_x, mud_weight_y, color='black', linewidth=2, linestyle='-', drawstyle='steps-post')  # Add the stepped mud_weight line to the second track
     
     #ax2 = plt3.twinx()
     #ax2.set_ylabel('MD')