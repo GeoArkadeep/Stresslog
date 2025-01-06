56bfb5a21e71a1febaad626b75aee1b4cfed72dc
Arkadeep Ghosh <160126374+GeoArkadeep@users.noreply.github.com>
2025-01-06 17:19:15 +0530

    Update conf.py
    fuck.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+# Add the parent directory to the Python path
+sys.path.insert(0, os.path.abspath('../../'))
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
