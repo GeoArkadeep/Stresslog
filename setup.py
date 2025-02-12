f73d558e2bced31ce7c752415778f6b95428f3b1
Arkadeep Ghosh <arkadeep_ghosh@rocklab.in>
2025-02-12 05:51:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
         "Topic :: Scientific/Engineering",
     ],
     python_requires=">=3.10,<3.12",
-    install_requires=["pandas>=2.0","numpy","scipy","welly","pint","matplotlib", "plotly"],
+    install_requires=["pandas>=2.0","numpy","scipy","welly","pint","matplotlib", "plotly","dlisio"],
     packages=setuptools.find_packages(),
     include_package_data=True,    
 )
